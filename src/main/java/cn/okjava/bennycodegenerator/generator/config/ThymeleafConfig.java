3e9e74db22264d95acfd28dacfcc39d587febdab
benny <xiaoc1314@163.com>
2019-09-18 15:13:54 +0800

    所有的模板初始化已完成

--- src/main/java/cn/okjava/bennycodegenerator/generator/config/ThymeleafConfig.java
+++ src/main/java/cn/okjava/bennycodegenerator/generator/config/ThymeleafConfig.java
@@ -1,14 +1,11 @@
 package cn.okjava.bennycodegenerator.generator.config;
 
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 
-import javax.annotation.Resource;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 
 /**
  * @author benny
@@ -16,15 +13,13 @@ import java.io.IOException;
  * description 模板引擎配置
  * @date 2019/9/16 10:02
  */
-@Component
-public class ThymeleafConfig {
+public enum ThymeleafConfig {
 
-    @Resource
-    private ResourceLoader resourceLoader;
+    INSTANCE;
 
-    private static TemplateEngine templateEngine;
+    private TemplateEngine templateEngine;
 
-    {
+    ThymeleafConfig() {
         FileTemplateResolver templateResolver = new FileTemplateResolver();
         templateResolver.setPrefix(getTemplatePath());
         templateResolver.setTemplateMode("TEXT");
@@ -33,19 +28,15 @@ public class ThymeleafConfig {
     }
 
     private String getTemplatePath() {
-        String fileName = "classpath:/templates/tmpl/";
         try {
-            System.out.println(new DefaultResourceLoader().getResource(fileName).getURI());
-        } catch (IOException e) {
-            e.printStackTrace();
+            // windows 平台下
+            return ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "templates" + File.separator + "tmpl").getPath() + File.separator;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("获取模板路径问题");
         }
-        // 获取静态资源文件夹目录 此种方式 linux 不生效
-        return getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "templates" + File.separator + "tmpl" + File.separator;
-        // windows 平台下
-        // return ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "templates" + File.separator + "tmpl").getPath() + File.separator;
     }
 
     public static TemplateEngine getTemplateEngine() {
-        return templateEngine;
+        return INSTANCE.templateEngine;
     }
 }
