67b2e50518307072d7c3bd0a458110180f01c63a
benny <xiaoc1314@163.com>
2019-09-18 15:24:35 +0800

    所有的模板初始化已完成

--- src/main/java/cn/okjava/bennycodegenerator/generator/config/ThymeleafConfig.java
+++ src/main/java/cn/okjava/bennycodegenerator/generator/config/ThymeleafConfig.java
@@ -1,11 +1,12 @@
 package cn.okjava.bennycodegenerator.generator.config;
 
+import cn.hutool.core.io.IoUtil;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.ResourceUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 
 /**
  * @author benny
@@ -29,6 +30,19 @@ public enum ThymeleafConfig {
 
     private String getTemplatePath() {
         try {
+            try {
+                ClassPathResource resource = new ClassPathResource("/tmpl/JpaEntity.benny");
+                OutputStream out = new ByteArrayOutputStream();
+                long copy = IoUtil.copy(resource.getInputStream(), out, IoUtil.DEFAULT_BUFFER_SIZE);
+                String result = "";
+                IoUtil.write(out, true, result.getBytes());
+                System.out.println("=====");
+                System.out.println(result);
+                System.out.println("=====");
+
+            } catch (IOException e) {
+                throw new RuntimeException("读取模板文件异常", e);
+            }
             // windows 平台下
             return ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "templates" + File.separator + "tmpl").getPath() + File.separator;
         } catch (FileNotFoundException e) {
